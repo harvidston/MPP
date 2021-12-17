fc917547ef3317ff8dd47d4dda0ad256b1da2e28
harvidston <harvidston@mail.ru>
2021-12-17 09:44:47 +0300

    3 & 4 tasks are ready

---
+++ 4 task (TestsGenerator)/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.TestGenerator.Implementations;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new NUnitTestGenerator(10, 10, 10);
+            generator.GenerateTestsAsync(@"z:\BSUIR\mpp4\input",
+                @"z:\BSUIR\mpp4\output").Wait();
+        }
+    }
+}
